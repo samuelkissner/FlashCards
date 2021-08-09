7c7bab1d2e5007bd6bd5d8da383318ff540a665a
samuelkissner <samuelkissner@gmail.com>
2021-08-09 13:25:31 -0500

    - added a Card class to store the concept (front of card) and description (back of card) - added logic to Main method to cycle through and randomly pick a Card instance within an Array List.

---
+++ FlashCards/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashCards
+{
+    class Card
+    {
+        public string Concept { get; set; }  //front of the card
+        public string Description { get; set; } //back of the card
+
+        public Card(string concept, string description)
+        {
+            this.Concept = concept;
+            this.Description = description;
+        }
+        
+    }
+}
--- FlashCards/Program.cs
+++ FlashCards/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace FlashCards
 {
@@ -6,7 +7,26 @@ namespace FlashCards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+ 
+            ArrayList flashCards = new ArrayList();
+            flashCards.Add(new Card("namespace", "container for a set of related classes and other namespaces (analagous to a package in Java)"));
+            flashCards.Add(new Card("Property", "Encapsulates a private field within a class. This private field can only be accessed from the getter and setter "));
+            flashCards.Add(new Card("C# Field", "A class-level variable that holds a value. Generally, it has the 'private' access modifier keyword applied to it."));
+            flashCards.Add(new Card("Literal Keywords", "keywords that apply to the current instance or value of an object. They include null, false, true, value, and void."));
+            flashCards.Add(new Card("Access Keywords", "keywords used to access the containing class or the base class of an object or class. They include this and base."));
+
+            Random random = new Random();
+            Card currentCard;
+            while (true)
+            {
+                int randomNumber = random.Next(0, flashCards.Count - 1);
+                currentCard = (Card) flashCards[randomNumber];
+
+                Console.WriteLine(currentCard.Concept);
+                Console.ReadLine();
+                Console.WriteLine(currentCard.Description);
+                Console.ReadLine();
+            }
         }
     }
 }
