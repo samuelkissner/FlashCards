b5c340745b92f356c1e76fcc8b8be27280293eae
samuelkissner <samuelkissner@gmail.com>
2021-08-10 13:29:51 -0500

    - Created a CardPile class to clean up the Main method. - Cards are now discarded after viewing them on the console

---
+++ CardPile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlashCards
+{
+    class CardPile
+    {
+        //fields/properties
+        public ArrayList Cards { get; set; }
+        private Random random; 
+
+        
+        //constructor
+        public CardPile ()
+        {
+            Cards = new ArrayList();
+            random = new Random();
+        }
+
+        //methods
+        public Card getRandomCard()
+        {
+            if (Cards.Count == 0)
+                return null;
+
+            int randomNumber = random.Next(0, Cards.Count - 1);
+            Card randomCard = (Card)Cards[randomNumber];
+            return randomCard;
+        }
+
+        public void discardCard(Card currentCard)
+        {
+            Cards.Remove(currentCard);
+        }
+        
+        public void CreateDefaultCardPile()
+        {
+            Cards.Add(new Card("namespace", "container for a set of related classes and other namespaces (analagous to a package in Java)"));
+            Cards.Add(new Card("Property", "Encapsulates a private field within a class. This private field can only be accessed from the getter and setter "));
+            Cards.Add(new Card("C# Field", "A class-level variable that holds a value. Generally, it has the 'private' access modifier keyword applied to it."));
+            Cards.Add(new Card("Literal Keywords", "keywords that apply to the current instance or value of an object. They include null, false, true, value, and void."));
+            Cards.Add(new Card("Access Keywords", "keywords used to access the containing class or the base class of an object or class. They include this and base."));
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,25 +8,29 @@ namespace FlashCards
         static void Main(string[] args)
         {
  
-            ArrayList flashCards = new ArrayList();
-            flashCards.Add(new Card("namespace", "container for a set of related classes and other namespaces (analagous to a package in Java)"));
-            flashCards.Add(new Card("Property", "Encapsulates a private field within a class. This private field can only be accessed from the getter and setter "));
-            flashCards.Add(new Card("C# Field", "A class-level variable that holds a value. Generally, it has the 'private' access modifier keyword applied to it."));
-            flashCards.Add(new Card("Literal Keywords", "keywords that apply to the current instance or value of an object. They include null, false, true, value, and void."));
-            flashCards.Add(new Card("Access Keywords", "keywords used to access the containing class or the base class of an object or class. They include this and base."));
-
-            Random random = new Random();
+            CardPile flashCards = new CardPile();
+            flashCards.CreateDefaultCardPile();
             Card currentCard;
+
+
             while (true)
             {
                 Console.Clear();
-                int randomNumber = random.Next(0, flashCards.Count - 1);
-                currentCard = (Card) flashCards[randomNumber];
+                currentCard = flashCards.getRandomCard();
+
+                if(currentCard == null)
+                {
+                    Console.WriteLine("The Card Pile is Empty!");
+                    Console.ReadLine();
+                    break;
+                }
 
                 Console.WriteLine(currentCard.Concept);
                 Console.ReadLine();
                 Console.WriteLine(currentCard.Description);
                 Console.ReadLine();
+
+                flashCards.discardCard(currentCard);
             }
         }
     }
